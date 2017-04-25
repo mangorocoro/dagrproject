e27b6df88deae5ed0f310379582585bb016902db
mangorocoro <daniel01001000@gmail.com>
2017-04-25 18:10:41 -0400

    options update to database connection

--- dagr/dagr/settings.py
+++ dagr/dagr/settings.py
@@ -82,15 +82,23 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
+
+
+
         'NAME': 'dagrbase',
         'USER': 'dagruser',
         'PASSWORD': 'password',
         'HOST': 'localhost',
         'PORT': '',
+
+
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/home/dan/djangoprojects/dagrproject/dagr/dagr/my.cnf',
+        },
     }
 }
 
