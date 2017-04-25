36968bd5bf42fe0e552414a4d5f197fd484f0871
mangorocoro <daniel01001000@gmail.com>
2017-04-24 20:32:18 -0400

    initial setup

---
+++ dagr/home/views.py
@@ -0,0 +1,10 @@
+# home/views.py
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'index.html', context=None)
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
