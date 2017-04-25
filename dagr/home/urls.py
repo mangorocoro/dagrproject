7c79d5aebc63494d5ed468a3b71de24dc43859ad
mangorocoro <daniel01001000@gmail.com>
2017-04-24 22:19:33 -0400

    templating structure and navigation

--- dagr/home/urls.py
+++ dagr/home/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import url
 from home import views
 
 urlpatterns = [
-    url(r'^$', views.HomePageView.as_view()),
-    url(r'^about/$', views.AboutPageView.as_view()),
+    url(r'^$', views.HomePageView.as_view(), name='home'),
+    url(r'^about/$', views.AboutPageView.as_view(), name='about'),
 ]
--- dagr/home/views.py
+++ dagr/home/views.py
@@ -3,8 +3,11 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
+    '''
     def get(self, request, **kwargs):
         return render(request, 'index.html', context=None)
-
+    '''
+    template_name = 'index.html'
+    
 class AboutPageView(TemplateView):
     template_name = 'about.html'
