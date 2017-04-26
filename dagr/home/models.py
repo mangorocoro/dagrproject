a4c4068a877a38cbf2c7a6e6466b1412a2536e0e
mangorocoro <daniel01001000@gmail.com>
2017-04-26 02:16:34 -0400

    debugging features for database models

--- dagr/home/models.py
+++ dagr/home/models.py
@@ -10,10 +10,19 @@ class DAGR(models.Model):
     docParent = models.CharField(max_length=255)
     docChild = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.GUID + " - " + self.docName
+
 class Categories(models.Model):
     category = models.CharField(max_length=255, primary_key=True)
     docID = models.ForeignKey(DAGR)
 
+    def __str__(self):
+        return self.docID + " - " + self.category
+
 class Keywords(models.Model):
     keywords = models.CharField(max_length=255, primary_key=True)
     docID = models.ForeignKey(DAGR)
+
+    def __str__(self):
+        return self.docID + " - " + self.keywords
