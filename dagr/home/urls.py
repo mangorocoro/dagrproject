e376afce6647df28d2c386d9fa3e4cc6884038db
mangorocoro <daniel01001000@gmail.com>
2017-04-27 02:59:11 -0400

    setup routing for all pages

--- dagr/home/urls.py
+++ dagr/home/urls.py
@@ -6,4 +6,14 @@ from home import views
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view(), name='home'),
     url(r'^about/$', views.AboutPageView.as_view(), name='about'),
+    url(r'^bulkdataentry/$', views.BulkEntryPageView.as_view(), name='bulkdataentry'),
+    url(r'^categorize/$', views.CategorizePageView.as_view(), name='categorize'),
+    url(r'^delete/$', views.DeletePageView.as_view(), name='delete'),
+    url(r'^findorphans/$', views.FindOrphansPageView.as_view(), name='findorphans'),
+    url(r'^htmlparser/$', views.HtmlParserPageView.as_view(), name='htmlparser'),
+    url(r'^insert/$', views.InsertPageView.as_view(), name='insert'),
+    url(r'^metadataquery/$', views.MetadataQueryPageView.as_view(), name='metadataquery'),
+    url(r'^modify/$', views.ModifyPageView.as_view(), name='modify'),
+    url(r'^reach/$', views.ReachPageView.as_view(), name='reach'),
+    url(r'^timerange/$', views.TimeRangePageView.as_view(), name='timerange'),
 ]
--- dagr/home/views.py
+++ dagr/home/views.py
@@ -12,3 +12,32 @@ class HomePageView(TemplateView):
 class AboutPageView(TemplateView):
     template_name = 'about.html'
 
+class BulkEntryPageView(TemplateView):
+    template_name = 'bulkentry.html'
+
+class CategorizePageView(TemplateView):
+    template_name = 'categorize.html'
+
+class DeletePageView(TemplateView):
+    template_name = 'delete.html'
+
+class FindOrphansPageView(TemplateView):
+    template_name = 'findorphans.html'
+
+class HtmlParserPageView(TemplateView):
+    template_name = 'htmlparser.html'
+
+class InsertPageView(TemplateView):
+    template_name = 'insert.html'
+
+class MetadataQueryPageView(TemplateView):
+    template_name = 'metadataquery.html'
+
+class ModifyPageView(TemplateView):
+    template_name = 'modify.html'
+
+class ReachPageView(TemplateView):
+    template_name = 'reach.html'
+
+class TimeRangePageView(TemplateView):
+    template_name = 'timerange.html'
